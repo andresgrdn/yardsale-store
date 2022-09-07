6a5d30f7665853d2bc032a3da7556b32bf2a93c5
Andres Gordon <gordon_andres@yahoo.es>
2022-09-07 18:59:53 -0500

    Added mobile menu

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const menuEmail = document.getElementsByClassName('account')[0];
-const desktopMenu = document.getElementsByClassName('desktop-menu')[0];
+const menuEmail = document.querySelector('.account');
+const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuHamIcon = document.querySelector('.mobile-menu-icon');
+const mobileMenuIcon = document.querySelector('.close-icon');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+mobileMenuIcon.addEventListener('click', toggleMobileMenu);
+
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle('inactive');
 }
