f0f6a49b50c8a7288b2ca5edef45b7a8073cf784
Andres Gordon <gordon_andres@yahoo.es>
2022-09-07 22:25:19 -0500

    Added shopping cart

--- index.js
+++ index.js
@@ -3,16 +3,41 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuHamIcon = document.querySelector('.mobile-menu-icon');
 const mobileMenuIcon = document.querySelector('.close-icon');
+const shoppingCartButton = document.querySelector('.cart-icon');
+const shoppingCart = document.querySelector('.product-description');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 mobileMenuIcon.addEventListener('click', toggleMobileMenu);
+shoppingCartButton.addEventListener('click', toggleShoppingCart);
 
 
 function toggleDesktopMenu() {
+  const isShoppingCartOpened = !shoppingCart.classList.contains('inactive');
+
+  if (isShoppingCartOpened) {
+    shoppingCart.classList.add('inactive');
+  }
+
   desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+  const isShoppingCartOpened = !shoppingCart.classList.contains('inactive');
+
+  if (isShoppingCartOpened) {
+    shoppingCart.classList.add('inactive');
+  }
+
   mobileMenu.classList.toggle('inactive');
+}
+
+function toggleShoppingCart() {
+  const isDesktopMenuOpened = !desktopMenu.classList.contains('inactive');
+
+  if (isDesktopMenuOpened) {
+    desktopMenu.classList.add('inactive');
+  }
+
+  shoppingCart.classList.toggle('inactive');
 }
