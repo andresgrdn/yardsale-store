bbba61140b506e3ccc4649e550999146a2552310
Andres Gordon <gordon_andres@yahoo.es>
2022-09-08 15:35:13 -0500

    Added product list

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const menuHamIcon = document.querySelector('.mobile-menu-icon');
 const mobileMenuIcon = document.querySelector('.close-icon');
 const shoppingCartButton = document.querySelector('.cart-icon');
 const shoppingCart = document.querySelector('.product-description');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -43,3 +44,96 @@ function toggleShoppingCart() {
 
   shoppingCart.classList.toggle('inactive');
 }
+
+const productsList = [];
+
+productsList.push({
+  name: 'Bike',
+  price: 120,
+  image: 'https://images.pexels.com/photos/1431117/pexels-photo-1431117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+})
+
+productsList.push({
+  name: 'Laptop',
+  price: 600,
+  image: 'https://images-na.ssl-images-amazon.com/images/I/61Dw5Z8LzJL._SL1000_.jpg',
+})
+
+productsList.push({
+  name: 'Table',
+  price: 150,
+  image: 'https://images.demandware.net/dw/image/v2/BBBV_PRD/on/demandware.static/-/Sites-master-catalog/default/dw04b3c19c/images/600000/606028.jpg?sfrm=jpg',
+})
+
+productsList.push({
+  name: 'Table',
+  price: 150,
+  image: 'https://images.demandware.net/dw/image/v2/BBBV_PRD/on/demandware.static/-/Sites-master-catalog/default/dw04b3c19c/images/600000/606028.jpg?sfrm=jpg',
+})
+
+productsList.push({
+  name: 'Table',
+  price: 150,
+  image: 'https://images.demandware.net/dw/image/v2/BBBV_PRD/on/demandware.static/-/Sites-master-catalog/default/dw04b3c19c/images/600000/606028.jpg?sfrm=jpg',
+})
+
+/*
+<div class="product-card" >
+  <img src="" alt="">
+
+  <div class="controls">
+    <p class="product-card__description">
+      <span class="product-card__price">$ 120,00</span>
+      <span class="product-card__name">Bike</span>
+    </p>
+
+    <figure class="product-card__icon">
+      <img src="./assets/icons/bt_add_to_cart.svg" alt="">
+    </figure>
+  </div>
+</div>
+*/
+
+function renderProducts(arr) {
+  for (const product of arr) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    const productImage = document.createElement('img');
+    productImage.setAttribute('src', product.image);
+    productImage.setAttribute('alt', product.name);
+
+    const controls = document.createElement('div');
+    controls.classList.add('controls');
+
+    const productDescription = document.createElement('p');
+    productDescription.classList.add('product-card__description');
+
+    const productPrice = document.createElement('span');
+    productPrice.classList.add('product-card__price');
+    productPrice.textContent = `$ ${product.price}`;
+
+    const productName = document.createElement('span');
+    productName.classList.add('product-card__name');
+    productName.textContent = product.name;
+
+    productDescription.append(productPrice, productName);
+
+    const cardIconContainer = document.createElement('figure');
+    cardIconContainer.classList.add('product-card__icon');
+
+    const cardIconImage = document.createElement('img');
+    cardIconImage.setAttribute('src', './assets/icons/bt_add_to_cart.svg');
+    cardIconImage.setAttribute('alt', 'Add to cart button');
+
+    cardIconContainer.appendChild(cardIconImage);
+
+    controls.append(productDescription, cardIconContainer);
+
+    productCard.append(productImage, controls);
+
+    cardsContainer.appendChild(productCard);
+  }
+}
+
+renderProducts(productsList);
